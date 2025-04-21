b82b82a40d29f682d813f08206c18d99ea32b42c
Duy Phuong <tdp100705@gmail.com>
2025-04-22 00:38:25 +0700

    sanpham

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ document.querySelectorAll('.heart-btn').forEach(button => {
         icon.classList.toggle('fa-solid');
     });
 });
+
+function toggleAccordion(element) {
+    element.classList.toggle("open");
+    const icon = element.querySelector(".icon");
+    icon.textContent = element.classList.contains("open") ? "–" : "+";
+  }
+  document.querySelector('.register-btn').addEventListener('click', () => {
+    window.open('https://example.com/register','_blank');
+  });
+  
+  document.querySelector('.add-to-cart').addEventListener('click', () => {
+    window.open('https://example.com/cart','_blank');
+  });
