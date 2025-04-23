7e253972e6e9eeb7201158a0d672c74b85d51401
Duy Phuong <tdp100705@gmail.com>
2025-04-23 23:48:43 +0700

    fix2

--- js/main.js
+++ js/main.js
@@ -306,3 +306,29 @@ class UserAuth {
 document.addEventListener('DOMContentLoaded', () => {
     new UserAuth();
 });
+
+document.querySelectorAll('.heart-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const icon = button.querySelector('.fa-heart');
+        icon.classList.toggle('fa-regular');
+        icon.classList.toggle('fa-solid');
+    });
+});
+
+function toggleAccordion(element) {
+    element.classList.toggle("open");
+    const icon = element.querySelector(".icon");
+    icon.textContent = element.classList.contains("open") ? "–" : "+";
+  }
+
+
+
+
+  
+  document.querySelector('.register-btn').addEventListener('click', () => {
+    window.open('https://example.com/register','_blank');
+  });
+  
+  document.querySelector('.add-to-cart').addEventListener('click', () => {
+    window.open('https://example.com/cart','_blank');
+  });
